236ae1662f1c7098f5bf41f3f1e2db73e63923b0
Magnus Cook <kimstone.yeager@gmail.com>
2022-05-18 16:36:11 +0200

    addtions for tutorial steps;

--- TestWebApp/TestWebApp/Controllers/LoginController.cs
+++ TestWebApp/TestWebApp/Controllers/LoginController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
 
 namespace TestWebApp.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Index()
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
         {
             return View();
         }
+
+        // GET: /<controller>/Login
+        public async Task<IActionResult> Login()
+        {
+            ViewData["Username"] = "Jacke the Dog";
+            ViewData["Password"] = "Finn the Human";
+            return View();
+        }
+
+        //// GET: /<controller>/Login
+        //public async Task<string> Login(string name, int count = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, Count is: {count}"); ;
+        //}
     }
 }
---
+++ TestWebApp/TestWebApp/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TestWebApp.Models
+{
+    public struct LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
