e1413ce0144771fcf360e1904b662933269db9f1
Magnus Cook <kimstone.yeager@gmail.com>
2022-05-20 09:44:51 +0200

    Check deps on nugets;

---
+++ SomeContractsNDataAsNuGet/SomeContractsNDataAsNuGet/DapperUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RandomDataGenerator;
+
+namespace SomeContractsNDataAsNuGet
+{
+    public class DapperUser
+    {
+        public DapperUser()
+        {
+        }
+        
+        public string RandomText(int length)
+        {
+            var gen = new RandomDataGenerator.Randomizers.RandomizerEmailAddress(new RandomDataGenerator.FieldOptions.FieldOptionsEmailAddress());
+            var text = gen.Generate();
+            return text;
+        }
+    }
+}
