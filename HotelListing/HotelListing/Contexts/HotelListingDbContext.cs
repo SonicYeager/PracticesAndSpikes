58cf48d9a6d5b338fb2f079767ce5032c7375077
SonicYeager <kimstone.yeager@gmail.com>
2022-07-02 02:21:25 +0200

    tweaks;

--- HotelListing/HotelListing/Contexts/HotelListingDbContext.cs
+++ HotelListing/HotelListing/Contexts/HotelListingDbContext.cs
@@ -3,22 +3,23 @@ using HotelListing.Models.Configurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace HotelListing.Contexts;
-
-public sealed class HotelListingDbContext : IdentityDbContext<UserEntity>
+namespace HotelListing.Contexts
 {
-    public HotelListingDbContext(DbContextOptions options) : base(options)
+    public sealed class HotelListingDbContext : IdentityDbContext<UserEntity>
     {
-    }
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+        }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        modelBuilder.ApplyConfiguration(new RoleConfiguration());
-        modelBuilder.ApplyConfiguration(new CountryConfiguration());
-        modelBuilder.ApplyConfiguration(new HotelConfiguration());
-    }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new CountryConfiguration());
+            modelBuilder.ApplyConfiguration(new HotelConfiguration());
+        }
 
-    public DbSet<HotelEntity> Hotels { get; set; }
-    public DbSet<CountryEntity> Countries { get; set; }
+        public DbSet<HotelEntity> Hotels { get; set; }
+        public DbSet<CountryEntity> Countries { get; set; }
+    }
 }
