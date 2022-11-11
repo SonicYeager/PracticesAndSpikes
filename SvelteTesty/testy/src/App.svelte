63228271a91148c57f068d2ae92526c596179845
SonicYeager <kimstone.yeager@gmail.com>
2022-11-11 20:00:57 +0100

    svelte further

--- SvelteTesty/testy/src/App.svelte
+++ SvelteTesty/testy/src/App.svelte
@@ -1,21 +1,31 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
-    import Card from './lib/Card.svelte';
+  import Card from './lib/Card.svelte';
+  import LoginForm from './lib/LoginForm.svelte';
+
+  let title = "Ich bims eins Testy";
+
+  function attemptLogin(username, password){
+    title = username + " " + password
+  }
 </script>
 
 <main>
-  <h1>Ich bims eins Testy</h1>
-
+  <link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" />
+  <h1>{title}</h1>
   <div class="card">
-    <Card cardTitle="I am a leftist!" cardSubtitle="Mine!"/>
-    <Card cardTitle="I am a rightist!" cardSubtitle="Mine!"/>
+    <Card>
+      <LoginForm onAttemptLogin={attemptLogin}/>
+    </Card>
   </div>
 </main>
 
 <style>
   .card {
     display: flex;
-    gap: 1em;
+    align-self: center;
+    align-content: center;
+    justify-content: center;
   }
   h1 {
     color: var(--primary-dark-color);
--- SvelteTesty/testy/src/lib/Card.svelte
+++ SvelteTesty/testy/src/lib/Card.svelte
@@ -1,61 +1,19 @@
 <script>
-export let cardTitle
-export let cardSubtitle
-export let cardContent = "{Card}"
 </script>
 
 <section>
-    <article class="cardHeading">
-        <h2>
-            {cardTitle}
-        </h2>
-        <h4>
-            {cardSubtitle}
-        </h4>
-    </article>
-    <article class="cardContent">
-        {cardContent}
-    </article>
+    <slot></slot>
 </section>
 
 <style>
     section {
         display: flex;
         flex-flow: row wrap;
-        height: 12em;
-        aspect-ratio: 1/1;
-        border-radius: 1em;
-        box-shadow: -1px 1px 3px whitesmoke;
-        background-color: var(--tertiary-dark-color);
-    }
-    h2, h4 {
-        color: var(--on-tertiary-dark-color);
-        margin: 0;
-    }
-    h4 {
-        font-style: italic;
-    }
-    .cardHeading {
-        display: flex;
-        flex-flow: column nowrap;
-        text-align: left;
-        margin: 0;
-        height: 4em;
-        width: 100%;
-        border-bottom: 1px solid whitesmoke;
-        padding: .5em;
-    }
-    .cardContent{
-        display: flex;
-        flex-flow: row wrap;
-        align-items: center;
+        border-radius: .5em;
+        background-color: whitesmoke;
+        gap: .5em;
         justify-content: center;
-        height: calc(100% - 5em);
-        width: 100%;
-        padding: .5em;
-    }
-    section:hover{
-        transform: scale(1.1);
-        transition: 300ms cubic-bezier(0.89,-0.43, 0.27, 1.35);
+        width: fit-content;
+        height: fit-content;
     }
 </style>
