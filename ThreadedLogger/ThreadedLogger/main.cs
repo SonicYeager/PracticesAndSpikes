5b49e9b1545a52cdc5da88745c47e05cb3c61ace
SonicYeager <69338558+SonicYeager@users.noreply.github.com>
2023-02-22 17:16:01 +0100

    empty

--- ThreadedLogger/ThreadedLogger/main.cs
+++ ThreadedLogger/ThreadedLogger/main.cs
@@ -2,73 +2,70 @@
 using Microsoft.Extensions.DependencyInjection;
 using AutoMapper;
 
-namespace ThreadedLogger
+namespace ThreadedLogger;
+public static class App
 {
-    public static class App
+
+    private static IMapper Mapper { get; set; } = null!;
+
+    private static IServiceProvider? ServiceProvider { get; set; }
+    private static IEnumerable<IDoSomethingWorker> DoSomethingWorkers { get; set; } = new List<IDoSomethingWorker>();
+
+    public static int Main(string[] argv)
     {
-        public static IEnumerable<string> Args { get; set; } = new List<string>();
+        ConfigureServices();
+        ConfigureAutoMapper();
 
-        public static IMapper Mapper { get; set; }
-
-        private static IServiceProvider ServiceProvider { get; set; }
-        public static IThreadedLogger ThreadedLogger { get; set; }
-        public static IEnumerable<IDoSomethingWorker> DoSomethingWorkers { get; set; } = new List<IDoSomethingWorker>();
-
-        public static int Main(string[] argv)
-        {
-            Args = new List<string>(argv);
-
-            ConfigureServices();
-            ConfigureAutoMapper();
-
-            DataPack dataPack = new DataPack() 
+        var dataPack = new DataPack() 
             { CalledCount = 3, Hash = 123456, Messages = new List<double>() { 2.00, 5.67, 8.432 } };
 
-            MessagePack messagePack = Mapper.Map<MessagePack>(dataPack);
+        var messagePack = Mapper.Map<MessagePack>(dataPack);
 
-            ThreadedLogger = ServiceProvider.GetService<IThreadedLogger>();
+        if (ServiceProvider != null)
+        {
+            ServiceProvider.GetService<IThreadedLogger>();
 
-            for (int i = 0; i < 10000; ++i)
+            for (var i = 0; i < 10000; ++i)
             {
-                ((List<IDoSomethingWorker>)DoSomethingWorkers).Add(ServiceProvider.GetService<IDoSomethingWorker>());
+                ((List<IDoSomethingWorker?>)DoSomethingWorkers).Add(ServiceProvider.GetService<IDoSomethingWorker>());
             }
-            foreach (var item in DoSomethingWorkers)
-            {
-                item.Begin();
-            }
+        }
+        foreach (var item in DoSomethingWorkers)
+        {
+            item.Begin();
+        }
 
-            Console.ReadLine();
+        Console.ReadLine();
 
-            foreach (var item in DoSomethingWorkers)
-            {
-                item.End();
-            }
+        foreach (var item in DoSomethingWorkers)
+        {
+            item.End();
+        }
             
-            return 0;
-        }
+        return 0;
+    }
 
-        private static void ConfigureServices()
-        {
-            var services = new ServiceCollection();
+    private static void ConfigureServices()
+    {
+        var services = new ServiceCollection();
 
-            services.AddSingleton<IThreadedLogger, ThreadedLogger>();
-            services.AddTransient<IDoSomethingWorker, DoSomethingWorker>();
+        services.AddSingleton<IThreadedLogger, ThreadedLogger>();
+        services.AddTransient<IDoSomethingWorker, DoSomethingWorker>();
 
-            ServiceProvider = services.BuildServiceProvider();
-        }
+        ServiceProvider = services.BuildServiceProvider();
+    }
 
-        private static void ConfigureAutoMapper()
-        {
-            var config = new MapperConfiguration(
-                cfg =>
-                {
-                    cfg.CreateMap<DataPack, MessagePack>();
-                    cfg.CreateMap<MessagePack, DataPack>();
-                });
+    private static void ConfigureAutoMapper()
+    {
+        var config = new MapperConfiguration(
+            cfg =>
+            {
+                cfg.CreateMap<DataPack, MessagePack>();
+                cfg.CreateMap<MessagePack, DataPack>();
+            });
 
-            config.AssertConfigurationIsValid();
+        config.AssertConfigurationIsValid();
 
-            Mapper = config.CreateMapper();
-        }
+        Mapper = config.CreateMapper();
     }
 }
