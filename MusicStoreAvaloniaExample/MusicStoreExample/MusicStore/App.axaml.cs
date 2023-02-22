44a899142ee156e51b49ff4a55310acfde35ae21
SonicYeager <69338558+SonicYeager@users.noreply.github.com>
2023-02-22 21:23:10 +0100

    added di without using it so far;

--- MusicStoreAvaloniaExample/MusicStoreExample/MusicStore/App.axaml.cs
+++ MusicStoreAvaloniaExample/MusicStoreExample/MusicStore/App.axaml.cs
@@ -1,15 +1,31 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Templates;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.DependencyInjection;
 using MusicStore.ViewModels;
 using MusicStore.Views;
+using System;
 
 namespace MusicStore;
-public partial class App : Application
+public class App : Application
 {
+    public IServiceProvider ServiceProvider { get; set; } = null!;
+    public IServiceCollection ServiceCollection { get; } = new ServiceCollection();
     public override void Initialize()
     {
+        ServiceCollection.AddTransient<AlbumViewModel>();
+        ServiceCollection.AddTransient<MusicStoreViewModel>();
+        ServiceCollection.AddTransient<MainWindowViewModel>();
+        ServiceCollection.AddTransient<AlbumView>();
+        ServiceCollection.AddTransient<MainWindow>();
+        ServiceCollection.AddTransient<MusicStoreView>();
+        ServiceCollection.AddTransient<MusicStoreWindow>();
+
+        ServiceProvider = ServiceCollection.BuildServiceProvider();
+        
         AvaloniaXamlLoader.Load(this);
+        DataTemplates.Add(new ViewLocator());
     }
 
     public override void OnFrameworkInitializationCompleted()
