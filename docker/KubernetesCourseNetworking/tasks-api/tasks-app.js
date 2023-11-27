a48b6d94421109bc3fa7d0713efa767b6c389afc
Tobias Wollrab <tobias.wollrab@relaxdays.de>
2023-11-27 15:27:09 +0100

    networking with kubernetes

--- docker/KubernetesCourseNetworking/tasks-api/tasks-app.js
+++ docker/KubernetesCourseNetworking/tasks-api/tasks-app.js
@@ -9,7 +9,12 @@ const filePath = path.join(__dirname, process.env.TASKS_FOLDER, 'tasks.txt');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+})
 
 const extractAndVerifyToken = async (headers) => {
   if (!headers.authorization) {
@@ -17,7 +22,7 @@ const extractAndVerifyToken = async (headers) => {
   }
   const token = headers.authorization.split(' ')[1]; // expects Bearer TOKEN
 
-  const response = await axios.get('http://auth/verify-token/' + token);
+  const response = await axios.get(`http://${process.env.AUTH_SERVICE_SERVICE_HOST}/verify-token/` + token);
   return response.data.uid;
 };
 
