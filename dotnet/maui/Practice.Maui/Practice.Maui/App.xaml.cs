0a9cfa83b27001516702673ee6d689ce53be4f11
SonicYeager <kimstone.yeager@gmail.com>
2023-12-01 19:56:15 +0100

    Update package references and remove unused imports
    Adjusted package versions in the project file and cleaned up the code by removing unused imports in various files. Also, the app icon was removed from the Android manifest file.

--- dotnet/maui/Practice.Maui/Practice.Maui/App.xaml.cs
+++ dotnet/maui/Practice.Maui/Practice.Maui/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Practice.Maui;
+﻿using Application = Microsoft.Maui.Controls.Application;
+
+namespace Practice.Maui;
 
 public sealed partial class App : Application
 {
--- dotnet/maui/Practice.Maui/Practice.Maui/Views/SettingsPage.xaml.cs
+++ dotnet/maui/Practice.Maui/Practice.Maui/Views/SettingsPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Practice.Maui.Views;
+﻿namespace Practice.Maui.Views;
 
 public partial class SettingsPage : ContentPage
 {
