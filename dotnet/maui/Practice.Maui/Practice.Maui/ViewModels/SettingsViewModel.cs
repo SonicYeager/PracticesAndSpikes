2b1bb0bab1d1a662b6fe038b0b7d2e746d506bbb
SonicYeager <kimstone.yeager@gmail.com>
2023-12-26 15:57:29 +0100

    Added Theme Toggle

--- dotnet/maui/Practice.Maui/Practice.Maui/ViewModels/SettingsViewModel.cs
+++ dotnet/maui/Practice.Maui/Practice.Maui/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace Practice.Maui.ViewModels;
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 
-public class SettingsViewModel
+namespace Practice.Maui.ViewModels;
+
+public sealed class SettingsViewModel
 {
-    
+    public ICommand ToggleTheme { get; }
+
+    public SettingsViewModel()
+    {
+        Application.Current!.UserAppTheme = Application.Current.RequestedTheme;
+        ToggleTheme = new RelayCommand(ToggleThemeAsync);
+    }
+
+    private static void ToggleThemeAsync()
+    {
+        if (Application.Current!.UserAppTheme == AppTheme.Dark)
+            Application.Current.UserAppTheme = AppTheme.Light;
+        else
+            Application.Current!.UserAppTheme = AppTheme.Dark;
+    }
 }
