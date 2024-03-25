d146645f5930060f9d0bf2f078c6ab57b9788836
SonicYeager <kimstone.yeager@gmail.com>
2024-03-25 20:09:50 +0100

    moving forward two chapers in the godot tutorial

---
+++ dotnet/godot/Assets/squash_the_creeps_start_1.1.0/squash_the_creeps_start_1.1.0/Player.cs
@@ -0,0 +1,55 @@
+using Godot;
+
+public partial class Player : CharacterBody3D
+{
+    // How fast the player moves in meters per second.
+    [Export]
+    public int Speed { get; set; } = 14;
+    // The downward acceleration when in the air, in meters per second squared.
+    [Export]
+    public int FallAcceleration { get; set; } = 75;
+
+    private Vector3 _targetVelocity = Vector3.Zero;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        var direction = Vector3.Zero;
+
+        if (Input.IsActionPressed("move_right"))
+        {
+            direction.X += 1.0f;
+        }
+        if (Input.IsActionPressed("move_left"))
+        {
+            direction.X -= 1.0f;
+        }
+        if (Input.IsActionPressed("move_back"))
+        {
+            direction.Z += 1.0f;
+        }
+        if (Input.IsActionPressed("move_forward"))
+        {
+            direction.Z -= 1.0f;
+        }
+
+        if (direction != Vector3.Zero)
+        {
+            direction = direction.Normalized();
+            GetNode<Node3D>("Pivot").Basis = Basis.LookingAt(direction);
+        }
+
+        // Ground velocity
+        _targetVelocity.X = direction.X * Speed;
+        _targetVelocity.Z = direction.Z * Speed;
+
+        // Vertical velocity
+        if (!IsOnFloor()) // If in the air, fall towards the floor. Literally gravity
+        {
+            _targetVelocity.Y -= FallAcceleration * (float)delta;
+        }
+
+        // Moving the character
+        Velocity = _targetVelocity;
+        MoveAndSlide();
+    }
+}
