3411b2fc910ff4329fd5edcdba592309e56e895b
SonicYeager <kimstone.yeager@gmail.com>
2024-03-29 21:41:22 +0100

    added mob spawning as per tutorial

---
+++ dotnet/godot/Assets/squash_the_creeps_start_1.1.0/squash_the_creeps_start_1.1.0/Main.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+    [Export] public PackedScene MobScene { get; set; }
+
+    // We also specified this function name in PascalCase in the editor's connection window
+    private void OnMobTimerTimeout()
+    {
+        // Create a new instance of the Mob scene.
+        var mob = MobScene.Instantiate<Mob>();
+
+        // Choose a random location on the SpawnPath.
+        // We store the reference to the SpawnLocation node.
+        var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/SpawnLocation");
+        // And give it a random offset.
+        mobSpawnLocation.ProgressRatio = GD.Randf();
+
+        var playerPosition = GetNode<Player>("Player").Position;
+        mob.Initialize(mobSpawnLocation.Position, playerPosition);
+
+        // Spawn the mob by adding it to the Main scene.
+        AddChild(mob);
+    }
+}
