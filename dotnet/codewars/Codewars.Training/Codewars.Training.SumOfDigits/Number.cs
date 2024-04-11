863cc97759b36609f39f31a1636addfa77187ae6
SonicYeager <kimstone.yeager@gmail.com>
2024-04-11 20:08:51 +0200

    another slightly more difficult one

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.SumOfDigits/Number.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Codewars.Training.SumOfDigits;
+
+public static class Number
+{
+    public static int DigitalRoot(long n)
+    {
+        var nAsString = n.ToString();
+        while (nAsString.Length > 1)
+        {
+            nAsString = nAsString.Select(static c => int.Parse(c.ToString())).Sum().ToString();
+        }
+
+        return int.Parse(nAsString);
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/SumOfDigitsTests.cs
@@ -0,0 +1,19 @@
+﻿using Codewars.Training.SumOfDigits;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public class SumOfDigitsTests
+{
+    [TestCase(0, ExpectedResult = 0)]
+    [TestCase(10, ExpectedResult = 1)]
+    [TestCase(16, ExpectedResult = 7)]
+    [TestCase(195, ExpectedResult = 6)]
+    [TestCase(992, ExpectedResult = 2)]
+    [TestCase(167346, ExpectedResult = 9)]
+    [TestCase(999999999999, ExpectedResult = 9)]
+    public int Tests(long n)
+    {
+        return Number.DigitalRoot(n);
+    }
+}
