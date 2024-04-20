29716c104c8baba241f704e2e44c7db33c874eeb
SonicYeager <kimstone.yeager@gmail.com>
2024-04-20 23:10:32 +0200

    added another kata

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.TenMinuteWalk/Kata.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace Codewars.Training.TenMinuteWalk;
+
+public static class Kata
+{
+    public static bool IsValidWalk(string[] walk)
+    {
+        if (walk.Length != 10) return false;
+
+        return walk
+                .Select(static w =>
+                {
+                    return w switch
+                    {
+                        "n" => (0, 1),
+                        "s" => (0, -1),
+                        "e" => (1, 0),
+                        "w" => (-1, 0),
+                        _ => (0, 0),
+                    };
+                })
+                .Aggregate(
+                    (0, 0),
+                    static (aggregated, elem) => (aggregated.Item1 + elem.Item1, aggregated.Item2 + elem.Item2))
+            is (0, 0);
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/TenMinuteWalkTests.cs
@@ -0,0 +1,39 @@
+﻿using Codewars.Training.TenMinuteWalk;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public sealed class TenMinuteWalkTests
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.Multiple(static () =>
+        {
+            Assert.That(Kata.IsValidWalk(new string[]
+                {
+                    "n", "s", "n", "s", "n", "s", "n", "s", "n", "s",
+                }),
+                Is.True,
+                "should return true");
+            Assert.That(Kata.IsValidWalk(new string[]
+                {
+                    "w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e",
+                }),
+                Is.False,
+                "should return false");
+            Assert.That(Kata.IsValidWalk(new string[]
+                {
+                    "w",
+                }),
+                Is.False,
+                "should return false");
+            Assert.That(Kata.IsValidWalk(new string[]
+                {
+                    "n", "n", "n", "s", "n", "s", "n", "s", "n", "s",
+                }),
+                Is.False,
+                "should return false");
+        });
+    }
+}
