63a037b701476e58b8f845ad0e88ad006388a6a7
SonicYeager <kimstone.yeager@gmail.com>
2024-04-20 13:27:06 +0200

    add sql kata

---
+++ sql/sql_bug_fixingt_the_join_kata.sql
@@ -0,0 +1,8 @@
+﻿SELECT j.job_title,
+       CAST(ROUND(SUM(j.salary) / COUNT(p.id), 2) AS FLOAT) AS average_salary,
+       COUNT(p.id)                                          AS total_people,
+       CAST(ROUND(SUM(j.salary), 2) AS FLOAT)               AS total_salary
+FROM people p
+         JOIN job j ON p.id = j.people_id
+GROUP BY j.job_title
+ORDER BY average_salary DESC;
