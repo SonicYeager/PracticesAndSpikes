b3e26bc1cccb180e8fc068f79df6cacd26d2f00c
SonicYeager <kimstone.yeager@gmail.com>
2024-04-23 18:06:04 +0200

    added another kata

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.DuplicateEncoder/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Codewars.Training.DuplicateEncoder;
+
+public static class Kata
+{
+    public static string DuplicateEncode(string word)
+    {
+        var lowered = word.ToLower();
+        var encoded = lowered.ToLower().Select(f => lowered.Count(c => f == c) > 1 ? ')' : '(');
+        return encoded.Aggregate("", static (current, c) => current + c);
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/DuplicateEncoderTests.cs
@@ -0,0 +1,19 @@
+﻿using Codewars.Training.DuplicateEncoder;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public class DuplicateEncoderTests
+{
+    [Test]
+    public void DuplicateEncode()
+    {
+        Assert.Multiple(static () =>
+        {
+            Assert.That(Kata.DuplicateEncode("din"), Is.EqualTo("((("));
+            Assert.That(Kata.DuplicateEncode("recede"), Is.EqualTo("()()()"));
+            Assert.That(Kata.DuplicateEncode("Success"), Is.EqualTo(")())())"), "should ignore case");
+            Assert.That(Kata.DuplicateEncode("(( @"), Is.EqualTo("))(("));
+        });
+    }
+}
