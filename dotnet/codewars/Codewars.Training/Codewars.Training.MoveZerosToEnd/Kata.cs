9b7aaf3897c08e924b299e58774d5da9f1c8d52b
Tobias Wollrab <tobias.wollrab@relaxdays.de>
2024-04-23 09:49:05 +0200

    added another kata

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.MoveZerosToEnd/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Codewars.Training.MoveZerosToEnd;
+
+public static class Kata
+{
+    public static int[] MoveZeroes(int[] arr)
+    {
+        var nonZeroes = arr.Where(static x => x != 0).ToArray();
+        var zeroes = arr.Where(static x => x == 0).ToArray();
+        return nonZeroes.Concat(zeroes).ToArray();
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/MoveZerosToEndTests.cs
@@ -0,0 +1,13 @@
+﻿using Codewars.Training.MoveZerosToEnd;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public class MoveZerosToEndTest
+{
+    [Test]
+    public void Test()
+    {
+        Assert.That(Kata.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }), Is.EqualTo(new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 }));
+    }
+}
