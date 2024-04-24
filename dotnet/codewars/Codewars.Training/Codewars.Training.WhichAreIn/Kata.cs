ca125021123baa9993e3a2d60c9a5be36d1f5f17
SonicYeager <kimstone.yeager@gmail.com>
2024-04-24 19:45:48 +0200

    added another kata

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.WhichAreIn/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Codewars.Training.WhichAreIn;
+
+public static class Kata
+{
+    public static string[] InArray(string[] array1, string[] array2)
+    {
+        var subStrings = array1
+            .Where(arr1 => array2.Any(arr2 => arr2.Contains(arr1)))
+            .OrderBy(static s => s)
+            .ToHashSet();
+        return subStrings.ToArray();
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/WhichAreInTests.cs
@@ -0,0 +1,44 @@
+﻿using Codewars.Training.WhichAreIn;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public class WhichAreInTests
+{
+
+    [Test]
+    public void InArray()
+    {
+        var a1 = new[]
+        {
+            "arp", "live", "strong",
+        };
+        var a2 = new[]
+        {
+            "lively", "alive", "harp", "sharp", "armstrong",
+        };
+        var r = new[]
+        {
+            "arp", "live", "strong",
+        };
+        Assert.That(Kata.InArray(a1, a2), Is.EqualTo(r));
+    }
+
+    [Test]
+    public void InArray_IsSortedLexicographically()
+    {
+        var a1 = new[]
+        {
+            "strong", "live", "arp",
+        };
+        var a2 = new[]
+        {
+            "lively", "alive", "harp", "sharp", "armstrong",
+        };
+        var r = new[]
+        {
+            "arp", "live", "strong",
+        };
+        Assert.That(Kata.InArray(a1, a2), Is.EqualTo(r));
+    }
+}
