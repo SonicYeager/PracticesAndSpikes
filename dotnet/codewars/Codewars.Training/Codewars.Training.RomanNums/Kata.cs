8efc190c65d38e753440f3e8c7998e5a10b67adf
SonicYeager <kimstone.yeager@gmail.com>
2024-05-05 19:08:35 +0200

    added another kata

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.RomanNums/Kata.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars.Training.RomanNums;
+
+public static class Kata
+{
+    private static readonly Dictionary<int, string> RomanDictionary = new Dictionary<int, string>
+    {
+        {
+            1000, "M"
+        },
+        {
+            900, "CM"
+        },
+        {
+            500, "D"
+        },
+        {
+            400, "CD"
+        },
+        {
+            100, "C"
+        },
+        {
+            90, "XC"
+        },
+        {
+            50, "L"
+        },
+        {
+            40, "XL"
+        },
+        {
+            10, "X"
+        },
+        {
+            9, "IX"
+        },
+        {
+            5, "V"
+        },
+        {
+            4, "IV"
+        },
+        {
+            1, "I"
+        }
+    };
+
+    public static string ToRoman(int n)
+    {
+        return RomanDictionary
+            .Where(dict => n >= dict.Key)
+            .Select(dict => dict.Value + ToRoman(n - dict.Key))
+            .FirstOrDefault();
+    }
+
+    public static int FromRoman(string romanNumeral)
+    {
+        return
+            romanNumeral.Length == 0
+                ? 0
+                : RomanDictionary
+                    .Where(dict => romanNumeral.StartsWith(dict.Value))
+                    .Select(dict => dict.Key + FromRoman(romanNumeral[dict.Value.Length..]))
+                    .FirstOrDefault();
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/RomanNumsTests.cs
@@ -0,0 +1,81 @@
+﻿using Codewars.Training.RomanNums;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public class RomanNumsTests
+{
+    [TestCase(1, "I")]
+    [TestCase(2, "II")]
+    [TestCase(3, "III")]
+    [TestCase(4, "IV")]
+    [TestCase(5, "V")]
+    [TestCase(6, "VI")]
+    [TestCase(7, "VII")]
+    [TestCase(8, "VIII")]
+    [TestCase(9, "IX")]
+    public void Roman_SingleDigits(int input, string expected)
+    {
+        var actual = Kata.ToRoman(input);
+        var actualBack = Kata.FromRoman(expected);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actualBack, Is.EqualTo(input));
+        });
+    }
+
+    [TestCase(10, "X")]
+    [TestCase(11, "XI")]
+    [TestCase(14, "XIV")]
+    [TestCase(15, "XV")]
+    [TestCase(16, "XVI")]
+    [TestCase(19, "XIX")]
+    [TestCase(40, "XL")]
+    [TestCase(90, "XC")]
+    public void Roman_DoubleDigits(int input, string expected)
+    {
+        var actual = Kata.ToRoman(input);
+        var actualBack = Kata.FromRoman(expected);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actualBack, Is.EqualTo(input));
+        });
+    }
+
+    [TestCase(100, "C")]
+    [TestCase(400, "CD")]
+    [TestCase(500, "D")]
+    [TestCase(900, "CM")]
+    public void Roman_TripleDigits(int input, string expected)
+    {
+        var actual = Kata.ToRoman(input);
+        var actualBack = Kata.FromRoman(expected);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actualBack, Is.EqualTo(input));
+        });
+    }
+
+    [TestCase(1000, "M")]
+    [TestCase(2000, "MM")]
+    [TestCase(3000, "MMM")]
+    [TestCase(1666, "MDCLXVI")]
+    [TestCase(1990, "MCMXC")]
+    public void Roman_QuadrupleDigits(int input, string expected)
+    {
+        var actual = Kata.ToRoman(input);
+        var actualBack = Kata.FromRoman(expected);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(actual, Is.EqualTo(expected));
+            Assert.That(actualBack, Is.EqualTo(input));
+        });
+    }
+}
