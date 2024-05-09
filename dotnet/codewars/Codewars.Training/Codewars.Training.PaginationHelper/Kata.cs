55d0be6fb1137276a7361080fd61fecfa7a20d5c
SonicYeager <kimstone.yeager@gmail.com>
2024-05-09 20:52:12 +0200

    added another kata

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.PaginationHelper/Kata.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codewars.Training.PaginationHelper;
+
+public sealed class PaginationHelper<T>
+{
+    private readonly IList<T> _collection;
+    private readonly int _itemsPerPage;
+
+    /// <summary>
+    /// Constructor, takes in a list of items and the number of items that fit within a single page
+    /// </summary>
+    /// <param name="collection">A list of items</param>
+    /// <param name="itemsPerPage">The number of items that fit within a single page</param>
+    public PaginationHelper(IList<T> collection, int itemsPerPage)
+    {
+        _collection = collection;
+        _itemsPerPage = itemsPerPage;
+    }
+
+    /// <summary>
+    /// The number of items within the collection
+    /// </summary>
+    public int ItemCount
+    {
+        get => _collection.Count;
+    }
+
+    /// <summary>
+    /// The number of pages
+    /// </summary>
+    public int PageCount
+    {
+        get
+        {
+            if (_itemsPerPage < 1)
+                return -1;
+
+            return (int)Math.Ceiling((double)_collection.Count / _itemsPerPage);
+        }
+    }
+
+    /// <summary>
+    /// Returns the number of items in the page at the given page index
+    /// </summary>
+    /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
+    /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
+    public int PageItemCount(int pageIndex)
+    {
+        if (pageIndex < 0 ||
+            pageIndex > PageCount - 1 ||
+            PageCount == 0)
+            return -1;
+
+        if (pageIndex == PageCount - 1 && _collection.Count % _itemsPerPage != 0)
+            return _collection.Count % _itemsPerPage;
+
+        return _itemsPerPage;
+    }
+
+    /// <summary>
+    /// Returns the page index of the page containing the item at the given item index.
+    /// </summary>
+    /// <param name="itemIndex">The zero-based index of the item to get the pageIndex for</param>
+    /// <returns>The zero-based page index of the page containing the item at the given item index or -1 if the item index is out of range</returns>
+    public int PageIndex(int itemIndex)
+    {
+        if (_collection.Count == 0 ||
+            itemIndex >= _collection.Count ||
+            itemIndex < 0)
+            return -1;
+
+        return itemIndex / _itemsPerPage;
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/PaginationHelperTests.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using Codewars.Training.PaginationHelper;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public class PaginationHelperTests
+{
+    [TestCaseSource(typeof(PageCountTestCases))]
+    public void PageCountTest(PaginationHelper<char> helper, int expected)
+    {
+        Assert.That(helper.PageCount, Is.EqualTo(expected));
+    }
+
+    [TestCaseSource(typeof(ItemCountTestCases))]
+    public void ItemCountTest(PaginationHelper<char> helper, int expected)
+    {
+        Assert.That(helper.ItemCount, Is.EqualTo(expected));
+    }
+
+
+    [TestCaseSource(typeof(PageItemCountTestCases))]
+    public int PageItemCountTest(PaginationHelper<char> helper, int pageIndex)
+    {
+        return helper.PageItemCount(pageIndex);
+    }
+
+    [TestCaseSource(typeof(PageIndexTestCases))]
+    public int PageIndexTest(PaginationHelper<char> helper, int itemIndex)
+    {
+        return helper.PageIndex(itemIndex);
+    }
+}
+
+file sealed class PageCountTestCases : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), -1), -1)
+            .SetName("EmptyCollectionAndNegativeItemsPerPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), 0), -1)
+            .SetName("EmptyCollectionAndZeroItemsPerPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), 1), 0)
+            .SetName("EmptyCollectionAndNItemsPerPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a',
+            }, 0), -1)
+            .SetName("SingleItemAndZeroItemsPerPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a',
+            }, 1), 1)
+            .SetName("SingleItemAndOneItemsPerPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a', 'b',
+            }, 1), 2)
+            .SetName("TwoItemsAndOneItemsPerPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+        {
+            'a', 'b',
+        }, 3), 1);
+    }
+}
+
+file sealed class ItemCountTestCases : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), 1), 0)
+            .SetName("EmptyCollectionAndOneItemsPerPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a',
+            }, 1), 1)
+            .SetName("SingleItemAndOneItemsPerPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a', 'b', 'c',
+            }, 1), 3)
+            .SetName("SingleItemAndOneItemsPerPage");
+    }
+}
+
+file sealed class PageItemCountTestCases : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), -1), 0)
+            .Returns(-1)
+            .SetName("EmptyCollectionWithNegativeItemsPerPageFirstPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), 0), 0)
+            .Returns(-1)
+            .SetName("EmptyCollectionWithZeroItemsPerPageFirstPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), 1), 0)
+            .Returns(-1)
+            .SetName("EmptyCollectionWithNItemsPerPageFirstPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a',
+            }, 0), 0)
+            .Returns(-1)
+            .SetName("SingleItemCollectionWithZeroItemsPerPageFirstPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a',
+            }, 1), 0)
+            .Returns(1)
+            .SetName("SingleItemCollectionWithSingleItemsPerPageFirstPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a',
+            }, 1), 1)
+            .Returns(-1)
+            .SetName("SingleItemCollectionWithSingleItemsPerPageSecondPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a', 'b', 'c',
+            }, 2), 1)
+            .Returns(1)
+            .SetName("ThreeItemCollectionWithTwoItemsPerPageSecondPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a', 'b', 'c', 'd',
+            }, 2), 1)
+            .Returns(2)
+            .SetName("FourItemCollectionWithTwoItemsPerPageSecondPage");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a', 'b', 'c', 'd',
+            }, 2), -1)
+            .Returns(-1)
+            .SetName("FourItemCollectionWithTwoItemsPerPageNegativePage");
+    }
+}
+
+file sealed class PageIndexTestCases : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), -1), -1)
+            .Returns(-1)
+            .SetName("EmptyCollectionWithNegativeItemsPerPageNegativeItemIndex");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), -1), 0)
+            .Returns(-1)
+            .SetName("EmptyCollectionWithNegativeItemsPerPageFirstItem");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), 0), 0)
+            .Returns(-1)
+            .SetName("EmptyCollectionWithZeroItemsPerPageFirstItem");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), 1), -1)
+            .Returns(-1)
+            .SetName("EmptyCollectionWithSingleItemsPerPageNegativeItem");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>(), 1), 0)
+            .Returns(-1)
+            .SetName("EmptyCollectionWithSingleItemsPerPageFirstItem");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a',
+            }, 1), 0)
+            .Returns(0)
+            .SetName("SingleItemCollectionWithSingleItemsPerPageFirstItem");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a',
+            }, 1), 1)
+            .Returns(-1)
+            .SetName("SingleItemCollectionWithSingleItemsPerPageSecondItem");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a', 'b',
+            }, 1), 1)
+            .Returns(1)
+            .SetName("TwoItemCollectionWithSingleItemsPerPageSecondItem");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a', 'b',
+            }, 2), 1)
+            .Returns(0)
+            .SetName("TwoItemCollectionWithTwoItemsPerPageSecondItem");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a', 'b', 'c',
+            }, 2), 2)
+            .Returns(1)
+            .SetName("ThreeItemCollectionWithTwoItemsPerPageThirdItem");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a', 'b', 'c', 'd',
+            }, 2), 2)
+            .Returns(1)
+            .SetName("FourItemCollectionWithTwoItemsPerPageThirdItem");
+        yield return new TestCaseData(new PaginationHelper<char>(new List<char>
+            {
+                'a', 'b', 'c', 'd',
+            }, 4), 2)
+            .Returns(0)
+            .SetName("FourItemCollectionWithFourItemsPerPageThirdItem");
+    }
+}
