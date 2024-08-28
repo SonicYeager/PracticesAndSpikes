56e718f3167d040284f5a2c062b99843f891909c
SonicYeager <kimstone.yeager@gmail.com>
2024-08-28 20:10:03 +0200

    added optional test parallelism

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿// In AssemblyInfo.cs or a separate file
+
+[assembly: Parallelizable(ParallelScope.Children)] //enable me when tests take more than 400 ms
+//[assembly: LevelOfParallelism(4)] // Adjust the number as needed
