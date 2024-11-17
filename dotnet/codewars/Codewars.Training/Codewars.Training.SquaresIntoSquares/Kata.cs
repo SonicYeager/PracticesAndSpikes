43675027d3df8f43cfaaf9d2ade715fc1fa7056a
SonicYeager <kimstone.yeager@gmail.com>
2024-11-17 16:58:16 +0100

    added another kata

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.SquaresIntoSquares/Kata.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Codewars.Training.SquaresIntoSquares;
+
+public static class Kata
+{
+    public static string? Decompose(long n)
+    {
+        var result = DecomposeHelper(n, n * n);
+        return result == null ? null : string.Join(" ", result);
+    }
+
+    private static List<long>? DecomposeHelper(long n, long remaining)
+    {
+        if (remaining == 0)
+            return new List<long>();
+
+        for (var i = n - 1; i > 0; i--)
+        {
+            var square = i * i;
+            if (remaining - square >= 0)
+            {
+                var result = DecomposeHelper(i, remaining - square);
+                if (result != null)
+                {
+                    result.Add(i);
+                    return result;
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/SquaresIntoSquaresTests.cs
@@ -0,0 +1,28 @@
+﻿using Codewars.Training.SquaresIntoSquares;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public sealed class SquaresIntoSquaresTests
+{
+    [Test]
+    public void Test1()
+    {
+        const long n = 11;
+        Assert.That(Kata.Decompose(n), Is.EqualTo("1 2 4 10"));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        const long n = 12;
+        Assert.That(Kata.Decompose(n), Is.EqualTo("1 2 3 7 9"));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        const long n = 7100;
+        Assert.That(Kata.Decompose(n), Is.EqualTo("2 3 5 119 7099")); //50410000
+    }
+}
