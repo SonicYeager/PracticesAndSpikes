b13ce76c0ecffcdb0854c5f3fc91f6c806e45086
SonicYeager <kimstone.yeager@gmail.com>
2024-11-17 15:16:59 +0100

    added another kata

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.MergedStringChecker/Kata.cs
@@ -0,0 +1,15 @@
+﻿namespace Codewars.Training.MergedStringChecker;
+
+public static class Kata
+{
+    public static bool isMerge(string s, string part1, string part2)
+    {
+        if (string.IsNullOrEmpty(s))
+            return string.IsNullOrEmpty(part1) && string.IsNullOrEmpty(part2);
+
+        var match1 = part1.Length > 0 && s[0] == part1[0] && isMerge(s[1..], part1[1..], part2);
+        var match2 = part2.Length > 0 && s[0] == part2[0] && isMerge(s[1..], part1, part2[1..]);
+
+        return match1 || match2;
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/MergedStringCheckerTests.cs
@@ -0,0 +1,37 @@
+﻿using Codewars.Training.MergedStringChecker;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public sealed class MergedStringCheckerTests
+{
+    [Test]
+    public void HappyPath1()
+    {
+        Assert.That(Kata.isMerge("codewars", "code", "wars"), Is.True);
+    }
+
+    [Test]
+    public void HappyPath2()
+    {
+        Assert.That(Kata.isMerge("codewars", "cdwr", "oeas"), Is.True);
+    }
+
+    [Test]
+    public void SadPath1()
+    {
+        Assert.That(Kata.isMerge("codewars", "cod", "wars"), Is.False);
+    }
+
+    [Test]
+    public void SadPath2()
+    {
+        Assert.That(Kata.isMerge("codewars", "codd", "wars"), Is.False);
+    }
+
+    [Test]
+    public void HappyPath4()
+    {
+        Assert.That(Kata.isMerge("Can we merge it? Yes, we can!", "n ee tYw n!", "Cawe mrgi? es, eca"), Is.True);
+    }
+}
