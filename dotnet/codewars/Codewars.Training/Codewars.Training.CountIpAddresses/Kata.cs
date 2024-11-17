8e69eefc71294601b90d23c774f6a0b772e7baa9
SonicYeager <kimstone.yeager@gmail.com>
2024-11-17 14:04:16 +0100

    added another kata

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.CountIpAddresses/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Codewars.Training.CountIpAddresses;
+
+public static class Kata
+{
+    public static long IpsBetween(string start, string end)
+    {
+        //parse segments
+        var startSegments = start.Split('.').Select(int.Parse).ToList();
+        var endSegments = end.Split('.').Select(int.Parse).ToList();
+
+        //calc diff at each segment and multiply by difference of segment and power of 2 beginning with ^0
+        return startSegments
+            .Zip(endSegments, static (s, e) => e - s)
+            .Aggregate(0L, static (acc, diff) => (acc << 8) + diff);
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/CountIpAddressesTest.cs
@@ -0,0 +1,18 @@
+﻿using Codewars.Training.CountIpAddresses;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public sealed class CountIpAddressesTest
+{
+    [Test]
+    public void SampleTest()
+    {
+        Assert.Multiple(static () =>
+        {
+            Assert.That(Kata.IpsBetween("10.0.0.0", "10.0.0.50"), Is.EqualTo(50));
+            Assert.That(Kata.IpsBetween("20.0.0.10", "20.0.1.0"), Is.EqualTo(246));
+            Assert.That(Kata.IpsBetween("0.0.0.0", "255.255.255.255"), Is.EqualTo((1L << 32) - 1L));
+        });
+    }
+}
