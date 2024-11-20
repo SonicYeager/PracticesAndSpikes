8c2dd75c8d4b18831a0687b8422ba6c52ebb0513
SonicYeager <kimstone.yeager@gmail.com>
2024-11-20 20:22:39 +0100

    resolved warning

--- dotnet/codewars/Codewars.Training/Codewars.Training.MergedStringChecker/Kata.cs
+++ dotnet/codewars/Codewars.Training/Codewars.Training.MergedStringChecker/Kata.cs
@@ -2,13 +2,13 @@
 
 public static class Kata
 {
-    public static bool isMerge(string s, string part1, string part2)
+    public static bool IsMerge(string s, string part1, string part2)
     {
         if (string.IsNullOrEmpty(s))
             return string.IsNullOrEmpty(part1) && string.IsNullOrEmpty(part2);
 
-        var match1 = part1.Length > 0 && s[0] == part1[0] && isMerge(s[1..], part1[1..], part2);
-        var match2 = part2.Length > 0 && s[0] == part2[0] && isMerge(s[1..], part1, part2[1..]);
+        var match1 = part1.Length > 0 && s[0] == part1[0] && IsMerge(s[1..], part1[1..], part2);
+        var match2 = part2.Length > 0 && s[0] == part2[0] && IsMerge(s[1..], part1, part2[1..]);
 
         return match1 || match2;
     }
