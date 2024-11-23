e942666b3dfb2f6b681b8f59f124ab5e56128ed1
SonicYeager <kimstone.yeager@gmail.com>
2024-11-23 01:10:34 +0100

    added another kata

---
+++ dotnet/codewars/Codewars.Training/Codewars.Training.TheObservedPin/Kata.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars.Training.TheObservedPin;
+
+public static class Kata
+{
+    private static readonly Dictionary<char, char[]> AdjacentDigits = new()
+    {
+        {
+            '1', [
+                '1', '2', '4',
+            ]
+        },
+        {
+            '2', [
+                '1', '2', '3', '5',
+            ]
+        },
+        {
+            '3', [
+                '2', '3', '6',
+            ]
+        },
+        {
+            '4', [
+                '1', '4', '5', '7',
+            ]
+        },
+        {
+            '5', [
+                '2', '4', '5', '6', '8',
+            ]
+        },
+        {
+            '6', [
+                '3', '5', '6', '9',
+            ]
+        },
+        {
+            '7', [
+                '4', '7', '8',
+            ]
+        },
+        {
+            '8', [
+                '5', '7', '8', '9', '0',
+            ]
+        },
+        {
+            '9', [
+                '6', '8', '9',
+            ]
+        },
+        {
+            '0', [
+                '0', '8',
+            ]
+        },
+    };
+
+    public static List<string> GetPINs(string observed)
+    {
+        var result = new List<string>
+        {
+            "",
+        };
+
+        foreach (var newResult in observed.Select(static digit => AdjacentDigits[digit]).Select(adjacent
+                     => (from prefix in result from suffix in adjacent select prefix + suffix).ToList<string>()))
+        {
+            result = newResult;
+        }
+
+        return result;
+    }
+}
---
+++ dotnet/codewars/Codewars.Training/Codewars.Training/TheObservedPinTests.cs
@@ -0,0 +1,37 @@
+﻿using Codewars.Training.TheObservedPin;
+
+namespace Codewars.Training;
+
+[TestFixture]
+public sealed class TheObservedPinTests
+{
+    [Test]
+    public void TestBasic()
+    {
+        var expectations = new Dictionary<string, string[]>
+        {
+            {
+                "8", [
+                    "5", "7", "8", "9", "0",
+                ]
+            },
+            {
+                "11", [
+                    "11", "22", "44", "12", "21", "14", "41", "24", "42",
+                ]
+            },
+            {
+                "369", [
+                    "339", "366", "399", "658", "636", "258", "268", "669", "668", "266", "369", "398", "256", "296", "259", "368", "638",
+                    "396", "238", "356", "659", "639", "666", "359", "336", "299", "338", "696", "269", "358", "656", "698", "699", "298",
+                    "236", "239",
+                ]
+            },
+        };
+
+        foreach (var pin in expectations)
+        {
+            Assert.That(Kata.GetPINs(pin.Key), Is.EquivalentTo(pin.Value), "PIN: " + pin);
+        }
+    }
+}
