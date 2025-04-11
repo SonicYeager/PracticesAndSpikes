83bb9ee57be55e0456a032786e4b374c11eb0c71
SonicYeager <kimstone.yeager@gmail.com>
2025-04-11 23:01:17 +0200

    continue js codewars

---
+++ javascript/codewars/digital-root/index.js
@@ -0,0 +1,13 @@
+﻿function digitalRoot(n) {
+    const digits = n.toString().split('').map(Number);
+
+    const sum = digits.reduce((acc, digit) => acc + digit, 0);
+
+    if (sum < 10) {
+        return sum;
+    }
+
+    return digitalRoot(sum);
+}
+
+export default digitalRoot;
---
+++ javascript/codewars/digital-root/test.js
@@ -0,0 +1,9 @@
+﻿import {assert} from 'chai';
+import digitalRoot from "./index.js";
+
+describe("Tests", () => {
+    it("test", () => {
+        assert.strictEqual(digitalRoot(16), 7)
+        assert.strictEqual(digitalRoot(456), 6)
+    });
+});
