b409debd37bc6e6889d13067db4906361a5b7807
SonicYeager <kimstone.yeager@gmail.com>
2025-04-19 17:58:25 +0200

    another rust kata

---
+++ rust/codewars-training/src/highest_and_lowest/mod.rs
@@ -0,0 +1,33 @@
+﻿pub fn high_and_low(numbers: &str) -> String {
+    let mut nums: Vec<i32> = numbers
+        .split_whitespace()
+        .filter_map(|s| s.parse().ok())
+        .collect();
+
+    if nums.is_empty() {
+        return String::new();
+    }
+
+    nums.sort_unstable();
+    let min = nums[0];
+    let max = nums[nums.len() - 1];
+
+    format!("{} {}", max, min)
+}
+
+
+#[cfg(test)]
+mod highest_and_lowest_tests
+{
+    use super::*;
+
+    #[test]
+    fn example_test_1() {
+        assert_eq!("42 -9", high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+    }
+
+    #[test]
+    fn example_test_2() {
+        assert_eq!("3 1", high_and_low("1 2 3"));
+    }
+}
--- rust/codewars-training/src/main.rs
+++ rust/codewars-training/src/main.rs
@@ -1,5 +1,6 @@
 mod circle_area_inside_square;
 mod credit_card_mask;
+mod highest_and_lowest;
 
 fn main() {
     println!("Just A CodeWars Project!");
