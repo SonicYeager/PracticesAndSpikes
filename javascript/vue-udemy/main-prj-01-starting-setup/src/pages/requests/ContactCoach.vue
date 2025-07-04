b6e81fc1c2a0520c065e5c450697a4a787452d6c
Tobias Wollrab <tobias.wollrab@relaxdays.com>
2025-07-04 09:03:36 +0200

    - Implement `ContactCoach` form with validation and error feedback   - Add email and message inputs with two-way binding.   - Validate inputs for email format and message length.   - Display error messages for invalid fields.   - Apply scoped styles for form layout and focus states.   - Integrate `BaseButton` component for submission.

--- javascript/vue-udemy/main-prj-01-starting-setup/src/pages/requests/ContactCoach.vue
+++ javascript/vue-udemy/main-prj-01-starting-setup/src/pages/requests/ContactCoach.vue
@@ -1,5 +1,83 @@
-﻿<script lang="ts" setup></script>
+﻿<script setup>
+import BaseButton from '@/components/ui/BaseButton.vue';
+import { ref } from 'vue';
 
-<template>CONTACT</template>
+const email = ref('');
+const message = ref('');
+const formIsValid = ref(true);
 
-<style scoped></style>
+function submitForm() {
+  formIsValid.value = true;
+
+  if (
+    email.value === '' ||
+    email.value.includes('@') ||
+    message.value.trim().length < 5
+  ) {
+    formIsValid.value = false;
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Your E-Mail</label>
+      <input id="email" v-model.trim="email" type="email" />
+    </div>
+    <div class="form-control">
+      <label for="message">Your Message</label>
+      <textarea id="message" v-model.trim="message" rows="5"></textarea>
+    </div>
+    <div class="actions">
+      <p v-if="!formIsValid" class="errors">
+        Please enter a valid email and a message with at least 5 characters.
+      </p>
+      <base-button type="submit">Send Message</base-button>
+    </div>
+  </form>
+</template>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
